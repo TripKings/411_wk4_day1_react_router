e845f5e139a754e11a20d7320d37481d9e7047a8
TripKings <antonio.cicchese@gmail.com>
2020-02-11 21:46:54 -0600

    finished

--- src/components/Car.js
+++ src/components/Car.js
@@ -1,12 +1,29 @@
 import React from 'react'
 import cars from '../cars.json'
+import { Container, Paper, Chip } from '@material-ui/core'
 // import material ui components here //
 // Container, Paper, Chip //
 
 const Car = (props) => {
+    var car = cars.find(car =>car.id==props.match.params.id)
     return (
-        <h1>A specific car</h1>
+    <Container>
+        <Paper>
+            <h1>{car.Name}</h1>
+        <Chip label = {`id: ${car.id}`}></Chip>
+        <Chip label = {`Name: ${car.Name}`}></Chip> 
+        <Chip label = {`Miles_per_Gallon: ${car.Miles_per_Gallon}`}></Chip>
+        <Chip label = {`Cylinders: ${car.Cylinders}`}></Chip>
+        <Chip label = {`Displacement: ${car.Displacement}`}></Chip> 
+        <Chip label = {`Horsepower: ${car.Horsepower}`}></Chip>
+        <Chip label = {`Weight_in_lbs: ${car.Weight_in_lbs}`}></Chip>
+        <Chip label = {`Acceleration: ${car.Acceleration}`}></Chip> 
+        <Chip label = {`Year: ${car.Year}`}></Chip>
+        <Chip label = {`Origin: ${car.origin}`}></Chip>
+        </Paper>
+    </Container> 
     )
 }
+
 
 export default Car
